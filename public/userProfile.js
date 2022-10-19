6342e55c6736e1113c7decdee0a8522dc4c2c916
Barry Cheung <vismkk@gmail.com>
2022-10-19 10:30:44 +0800

    register, login and userprofile frontend updated

--- public/userProfile.js
+++ public/userProfile.js
@@ -1,15 +1,53 @@
 
 
+// below code for userProfile redirect page
+window.onload = () => {
+    init()
+}
+
+function init() {
+    document.querySelector('#diginmon-park')
+        .addEventListener('click', DigimonParkPage)
+    document.querySelector('#diginmon-profile')
+        .addEventListener('click', DigimonProfilePage)
+    document.querySelector('#battle-history')
+        .addEventListener('click', battleHistoryPage)
+    document.querySelector('#logout')
+        .addEventListener('click', logout)
+}
+
+async function DigimonParkPage() {
+    window.location.href = 'digimon.html'
+}
+
+async function DigimonProfilePage() {
+    window.location.href = '/monster-page/digimon-detail.html'
+}
+
+async function battleHistoryPage() {
+    window.location.href = '/battle-history/battle-history.html'
+}
+
+async function logout() {
+    const res = await fetch(`/user/logout`, {
+        method: "POST"
+    })
+    console.log(res)
+    if (res.ok) {
+        window.location.href = '/registerAndLogin.html'
+    }
+}
+
 // below code for QR code
 const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
+    qrInput = wrapper.querySelector(".form input"),
+    generateBtn = wrapper.querySelector(".form button"),
+    qrImg = wrapper.querySelector(".qr-code img");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
-    if(!qrValue || preValue === qrValue) return;
+    if (!qrValue || preValue === qrValue) return;
     preValue = qrValue;
     generateBtn.innerText = "Generating QR Code...";
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
@@ -20,7 +58,7 @@ generateBtn.addEventListener("click", () => {
 });
 
 qrInput.addEventListener("keyup", () => {
-    if(!qrInput.value.trim()) {
+    if (!qrInput.value.trim()) {
         wrapper.classList.remove("active");
         preValue = "";
     }
